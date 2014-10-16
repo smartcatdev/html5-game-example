fc9a6b40c8cecadb438e2eec5fe75f5769007f13
bilal.hassan <info@ottawapc.ca>
2014-10-16 16:25:11 -0400

    completed project

--- Preloader.js
+++ Preloader.js
@@ -14,6 +14,16 @@ BunnyDefender.Preloader.prototype = {
 		this.titleText.anchor.setTo(0.5, 0.5);
         this.load.image('titlescreen', 'images/TitleBG.png');
         this.load.bitmapFont('eightbitwonder', 'fonts/eightbitwonder.png', 'fonts/eightbitwonder.fnt');
+        this.load.image('hill', 'images/hill.png');
+        this.load.image('sky', 'images/sky.png');
+        this.load.atlasXML('bunny', 'images/spritesheets/bunny.png', 'images/spritesheets/bunny.xml');
+        this.load.atlasXML('spacerock', 'images/spritesheets/SpaceRock.png', 'images/spritesheets/SpaceRock.xml');
+        this.load.image('explosion', 'images/explosion.png');
+        this.load.image('ghost', 'images/ghost.png');
+        this.load.audio('explosion_audio', 'audio/explosion.mp3');
+        this.load.audio('hurt_audio', 'audio/hurt.mp3');
+        this.load.audio('select_audio', 'audio/select.mp3');
+        this.load.audio('game_audio', 'audio/bgm.mp3');
 	},
 
 	create: function () {
@@ -21,7 +31,9 @@ BunnyDefender.Preloader.prototype = {
 	},
 
 	update: function () {
-	   	this.ready = true;
-        this.state.start('StartMenu');
+        if(this.cache.isSoundDecoded('game_audio') && this.ready == false) {
+            this.ready = true;
+            this.state.start('StartMenu');
+        }
 	}
 };
--- StartMenu.js
+++ StartMenu.js
@@ -1,11 +1,14 @@
 BunnyDefender.StartMenu = function(game) {
     this.startBG;
     this.startPrompt;
+    this.ding;
 }
 
 BunnyDefender.StartMenu.prototype = {
 	
 	create: function () {
+        this.ding = this.add.audio('select_audio');
+        
 		startBG = this.add.image(0, 0, 'titlescreen');
 		startBG.inputEnabled = true;
 		startBG.events.onInputDown.addOnce(this.startGame, this);
@@ -14,6 +17,7 @@ BunnyDefender.StartMenu.prototype = {
 	},
 
 	startGame: function (pointer) {
+        this.ding.play();
 		this.state.start('Game');
 	}
 };
